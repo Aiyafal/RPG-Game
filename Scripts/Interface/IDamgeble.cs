24038205834267e8c832f627da4ad0cef2cca602
Aiyafal <1421934268@qq.com>
2026-06-06 12:43:46 +0800

    Delete Scripts directory

--- Scripts/Interface/IDamgeble.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IDamgeble
-{
-    public void TakeDamage(float damage, Transform damageDealer);
-}
